5873562de40a743f4c574fa8cce513f05b262921
郭玉明 <857135447@qq.com>
2019-11-20 21:27:42 +0800

    每日两道程序题

---
+++ day21/最小公倍数.cpp
@@ -0,0 +1,21 @@
+/*最小公倍数*/
+
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b) { 
+	int r;
+	while(r = a % b) {
+		a = b;
+		b = r;
+	} 
+	return b;
+} 
+
+int main() { 
+	int a, b;
+	while(cin >> a >> b) {
+		cout << a * b / gcd(a, b) << endl;
+	}
+	return 0;
+} 
