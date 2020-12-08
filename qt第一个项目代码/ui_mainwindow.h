ded3234215baa9069086d89b56cfa24ff0f66837
BaomingRose <49124801+BaomingRose@users.noreply.github.com>
2020-12-08 21:26:54 +0800

    qt代码分析

---
+++ qt第一个项目代码/ui_mainwindow.h
@@ -0,0 +1,87 @@
+﻿/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QLabel *label;
+    QPushButton *clsButton;
+    QMenuBar *menubar;
+    QMenu *menumain;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(572, 283);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(220, 80, 141, 41));
+        QFont font;
+        font.setFamily(QString::fromUtf8("Times New Roman"));
+        font.setPointSize(12);
+        label->setFont(font);
+        clsButton = new QPushButton(centralwidget);
+        clsButton->setObjectName(QString::fromUtf8("clsButton"));
+        clsButton->setGeometry(QRect(360, 150, 101, 51));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 572, 23));
+        menumain = new QMenu(menubar);
+        menumain->setObjectName(QString::fromUtf8("menumain"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menumain->menuAction());
+
+        retranslateUi(MainWindow);
+        QObject::connect(clsButton, SIGNAL(clicked()), MainWindow, SLOT(close()));
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Hello,world", nullptr));
+        clsButton->setText(QCoreApplication::translate("MainWindow", "Close", nullptr));
+        menumain->setTitle(QCoreApplication::translate("MainWindow", "main", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
